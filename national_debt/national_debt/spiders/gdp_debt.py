46879ee591f617e502f2abbad0bd4c95bef6b0f5
RitaJain <prayasjain1996@gmail.com>
2020-05-06 00:10:02 +0530

    crawl worldometer

---
+++ national_debt/national_debt/spiders/gdp_debt.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GdpDebtSpider(scrapy.Spider):
+    name = 'gdp_debt'
+    allowed_domains = ['www.worldpopulationreview.com']
+    start_urls = ['http://www.worldpopulationreview.com/countries/countries-by-national-debt/']
+
+    def parse(self, response):
+        pass
