cbfe6e5deb5d0e5fb974c7c045e72dac55edb75a
RitaJain <prayasjain1996@gmail.com>
2020-05-06 00:28:01 +0530

    crawled national_debt

--- national_debt/national_debt/spiders/gdp_debt.py
+++ national_debt/national_debt/spiders/gdp_debt.py
@@ -3,9 +3,16 @@ import scrapy
 
 
 class GdpDebtSpider(scrapy.Spider):
-    name = 'gdp_debt'
-    allowed_domains = ['www.worldpopulationreview.com']
-    start_urls = ['http://www.worldpopulationreview.com/countries/countries-by-national-debt/']
+  name = 'gdp_debt'
+  allowed_domains = ['www.worldpopulationreview.com']
+  start_urls = ['https://www.worldpopulationreview.com/countries/countries-by-national-debt/']
 
-    def parse(self, response):
-        pass
+  def parse(self, response):
+    countries = response.xpath('//table[@class="datatableStyles__StyledTable-bwtkle-1 hOnuWY table table-striped"]/tbody/tr')
+    for country in countries:
+      name = country.xpath('.//td[1]/a/text()').get()
+      debt_ratio = country.xpath('.//td[2]/text()').get()
+      yield {
+        'country': name,
+        'debt_ratio': debt_ratio
+      }
